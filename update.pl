43af5bc3dbdb65d449f5a46496181f5815714859
Audrey Tang <audreyt@audreyt.org>
2014-07-15 11:21:38 +0000

    * Autoupdater for 10mins while gitbooks.io is down

---
+++ update.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use FindBin '$Bin';
+while (1) {
+    print "Checking build\n";
+    system("git fetch origin");
+    system("./deploy") if (`git log HEAD.. --oneline` =~ /\S/);
+    sleep 600;
+}
